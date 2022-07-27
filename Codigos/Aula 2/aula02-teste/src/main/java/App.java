39795c16ca27956f9841040f94b425358b35da02
Joao Delfino <joaohsdelfino@gmail.com>
2022-07-27 10:14:05 -0300

    Adicionado material à aula 2.

---
+++ Codigos/Aula 2/aula02-teste/src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+
+        int valor;
+
+
+        double pi = 3.14159;
+
+
+        valor = 2;
+        System.out.println(valor);
+        System.out.println(pi);
+    }
+}
